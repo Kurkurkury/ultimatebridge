c1428650b256edc885901a207a51f9a136653de6
Kurkurkury <hallowelt1432@gmx.ch>
2026-07-06 16:44:20 +0200

    Add extension native request handlers

--- extension/src/service-worker/index.js
+++ extension/src/service-worker/index.js
@@ -1,13 +1,24 @@
 import { sendNativeMessage } from '../transport/native-client.js';
 
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-  if (message?.type !== 'ULTIMATEBRIDGE_REQUEST_DETECTED') {
-    return false;
+  if (message?.type === 'ULTIMATEBRIDGE_REQUEST_DETECTED') {
+    sendNativeMessage({ body: message.body })
+      .then((response) => sendResponse({ ok: true, response }))
+      .catch((error) => sendResponse({ ok: false, error: error.message }));
+    return true;
   }
 
-  sendNativeMessage({ body: message.body })
-    .then((response) => sendResponse({ ok: true, response }))
-    .catch((error) => sendResponse({ ok: false, error: error.message }));
+  if (message?.type === 'ULTIMATEBRIDGE_RUN_READONLY_SMOKE') {
+    const body = [
+      'AUTO_BRIDGE_REQUEST_ID=AUTO',
+      '& .\\runner\\powershell\\UB_BeginTask.ps1 HealthCheck'
+    ].join('\n');
 
-  return true;
+    sendNativeMessage({ body })
+      .then((response) => sendResponse({ ok: true, response }))
+      .catch((error) => sendResponse({ ok: false, error: error.message }));
+    return true;
+  }
+
+  return false;
 });
